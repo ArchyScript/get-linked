51180c6f731634a7a0d441ed618e0704788e4453
ArchyScript <yungdansa@gmail.com>
2023-06-08 17:38:16 +0100

    basic setup

---
+++ layouts/dashboard.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="h-screen flex justify-evenly w-screen">
+    <div
+      class="flex w-full md:w-1/2 justify-center items-center h-full bg-white"
+    >
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">   
+</script>
