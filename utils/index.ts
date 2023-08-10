50936855210bd06b931aa6e5e9d3b10f11470d06
ArchyScript <yungdansa@gmail.com>
2023-08-10 09:10:17 +0100

    consumed auth api

--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,19 @@
 // format date
-const formatDate = (data: String, result_type: String) => {
+export const formatDate = (data: String, result_type: String) => {
   console.log('data:::', data)
   console.log('result_type:::', result_type)
+}
+
+//
+export const pasteHandler = (event: Event) => {
+  event.preventDefault() // Prevent the default paste behavior
+  const clipboardData = (event as ClipboardEvent).clipboardData || window.clipboardData
+  const pastedText = clipboardData.getData('text/plain')
+  console.log(pastedText)
+  // Do whatever validation or processing you need to do with the pasted text
+}
+
+//
+export const dropHandler = (event: Event) => {
+  event.preventDefault() // Prevent the default drop behavior
 }
